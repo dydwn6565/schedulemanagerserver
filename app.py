9910398687b73f0fa8eca5d33f02a51176ada41d
dydwn6565 <55012173+dydwn6565@users.noreply.github.com>
2022-09-19 15:10:00 -0700

    updated main

--- app.py
+++ app.py
@@ -83,4 +83,5 @@ if __name__ == '__main__':
     from db import db
     db.init_app(app)
     
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
